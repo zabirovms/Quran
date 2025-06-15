d1db751ede912d05a109a2291cf8b6c1883eb201
Muhammadsharif <zabirovms@gmail.com>
2025-06-15 14:20:19 +0500

    feat: add Android project structure for AAB build

---
+++ android/app/src/main/java/com/quran/tajik/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.quran.tajik
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            setSupportZoom(true)
+            builtInZoomControls = true
+            displayZoomControls = false
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://quran.tj")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+} 
